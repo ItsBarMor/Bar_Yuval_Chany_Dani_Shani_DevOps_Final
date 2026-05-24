f4ea89b13e8f7ba862d283febff49867ba680ab3
Daniel <daniel@example.com>
2026-05-24 12:49:14 +0300

    Add Jenkins deployment script

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+@echo off
+echo "Deploying to Tomcat..."
+xcopy /E /I /Y "daniel-yuval-bar-chany-shani" "C:\Users\daniel.gorodnitskiy\Tomcat\apache-tomcat-9.0.118\webapps\daniel-yuval-bar-chany-shani"
+echo "Deployment Complete!"
